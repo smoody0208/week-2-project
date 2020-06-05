c7deaf6cf5ab540121f804880c847eec5af4293c
Spencer Moody <spencer.moody@outlook.com>
2020-06-05 15:05:08 -0700

    Add counters to show each programming language popularity and else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -10,16 +10,86 @@ $(document).ready(function(){
 
     $(".name").text(nameInput);
 
-    if (question1 === "medium" && question2 === "fun" && question3 === "highschool" && question4 === "web" && question5 === "no") {
+    let rubyTotal = 0;
+    let jsTotal = 0;
+    let cSharpTotal = 0;
+
+    if (question1 === "a"){
+      cSharpTotal +=1;
+    } else if (question1 === "b"){
+      jsTotal +=1;
+      rubyTotal +=1;
+    } else if (question1 === "c"){
+      cSharpTotal +=1;
+      jsTotal +=1;
+    }
+
+    if (question2 === "a"){
+      cSharpTotal +=1;
+    } else if (question2 === "b"){
+      jsTotal +=1;
+      rubyTotal +=1;
+    } else if (question2 === "c"){
+      jsTotal +=1;
+    }
+
+    if (question3 === "a"){
+      cSharpTotal +=1;
+    } else if (question3 === "b"){
+      jsTotal +=1;
+    } else if (question3 === "c"){
+      jsTotal +=1;
+      cSharpTotal +=1;
+      rubyTotal +=1;
+    }
+
+    if (question4 === "a"){
+      cSharpTotal +=1;
+    } else if (question4 === "b"){
+      jsTotal +=1;
+      rubyTotal +=1;
+    } else if (question4 === "c"){
+      jsTotal +=1;
+    }
+
+    if (question5 === "a"){
+      cSharpTotal +=1;
+    } else if (question5 === "b"){
+      jsTotal +=1;
+      rubyTotal +=1;
+    } else if (question5 === "c"){
+      jsTotal +=1;
+    }
+
+    if (cSharpTotal === 0 && jsTotal === 0 && rubyTotal === 0){
+      alert("please select your answers")
+    } else if (cSharpTotal > rubyTotal && cSharpTotal > jsTotal){
+      $("#a-csharp").show();
+      $("#quizForm").hide();
+    } else if (rubyTotal > cSharpTotal && rubyTotal > jsTotal){
+      $("#a-ruby").show();
+      $("#quizForm").hide();
+    } else if (jsTotal > cSharpTotal && jsTotal > jsTotal){
+      $("#jsTotal").show();
+      $("#quizForm").hide();
+    } else {
+      $("#cSharp").show();
+      $("#quizForm").hide();
+    } 
+
+
+
+
+    /*if (question1 === "medium" && question2 === "fun" && question3 === "highschool" || "tradeschool" && question4 === "web" || "app" && question5 === "no") {
       $("#a-js").show();
       $("#quizForm").hide();
-    } else if (question1 === "small" && question2 === "change" && question3 === "highschool" && question4 === "web" && question5 === "yes"){
+    } else if (question1 === "small" || "medium" && question2 === "change" && question3 === "highschool" && question4 === "web" && question5 === "yes"){
       $("#a-ruby").show();
       $("#quizForm").hide();
     } else {
       $("#a-csharp").show();
       $("#quizForm").hide();
-    }
+    }*/
   })
 
 })
