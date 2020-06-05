210a5d2789dbf2cb56b7c848d01b5fc1bf366c73
Spencer Moody <spencer.moody@outlook.com>
2020-06-05 13:03:55 -0700

    Add branching to determine best programming language

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,19 @@ $(document).ready(function(){
     const question3 = $("input:radio[name=question3]:checked").val();
     const question4 = $("input:radio[name=question4]:checked").val();
     const question5 = $("input:radio[name=question5]:checked").val();
+
+    $(".name").text(nameInput);
+
+    if (question1 === "medium" && question2 === "fun" && question3 === "highschool" || "tradeschool" && question4 === "web" && question5 === "no") {
+      $("#a-js").show();
+      $("#quizForm").hide();
+    } else if (question1 === "small" && question2 === "change" && question3 === "highschool" && question4 === "web" && question5 === "yes"){
+      $("#a-ruby").show();
+      $("#quizForm").hide();
+    } else {
+      $("#a-csharp").show();
+      $("#quizForm").hide();
+    }
   })
+
 })
